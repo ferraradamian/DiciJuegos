1fa635a3162f37ed36745052502f6f329a161711
ferraradamian <ferraradamian@gmail.com>
2016-08-27 20:28:00 -0300

    first check in

---
+++ DiciJuegos/CapaPresentacion/FrmInicio.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLPFondoInicio = new System.Windows.Forms.TableLayoutPanel();
+            this.MSInicio = new System.Windows.Forms.MenuStrip();
+            this.aBMsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuracToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.palabrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.institucionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PBUtnFooter = new System.Windows.Forms.PictureBox();
+            this.PContenedor = new System.Windows.Forms.Panel();
+            this.TLPFondoInicio.SuspendLayout();
+            this.MSInicio.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBUtnFooter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TLPFondoInicio
+            // 
+            this.TLPFondoInicio.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.TLPFondoInicio.ColumnCount = 1;
+            this.TLPFondoInicio.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TLPFondoInicio.Controls.Add(this.MSInicio, 0, 0);
+            this.TLPFondoInicio.Controls.Add(this.PBUtnFooter, 0, 2);
+            this.TLPFondoInicio.Controls.Add(this.PContenedor, 0, 1);
+            this.TLPFondoInicio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TLPFondoInicio.Location = new System.Drawing.Point(0, 0);
+            this.TLPFondoInicio.Name = "TLPFondoInicio";
+            this.TLPFondoInicio.RowCount = 3;
+            this.TLPFondoInicio.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.TLPFondoInicio.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TLPFondoInicio.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.TLPFondoInicio.Size = new System.Drawing.Size(905, 643);
+            this.TLPFondoInicio.TabIndex = 0;
+            // 
+            // MSInicio
+            // 
+            this.MSInicio.BackColor = System.Drawing.Color.SkyBlue;
+            this.MSInicio.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBMsToolStripMenuItem,
+            this.loginToolStripMenuItem,
+            this.configuracToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.MSInicio.Location = new System.Drawing.Point(0, 0);
+            this.MSInicio.Name = "MSInicio";
+            this.MSInicio.Size = new System.Drawing.Size(905, 24);
+            this.MSInicio.TabIndex = 0;
+            this.MSInicio.Text = "menuStrip1";
+            // 
+            // aBMsToolStripMenuItem
+            // 
+            this.aBMsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosToolStripMenuItem,
+            this.palabrasToolStripMenuItem,
+            this.categoriaToolStripMenuItem,
+            this.regionesToolStripMenuItem,
+            this.institucionesToolStripMenuItem});
+            this.aBMsToolStripMenuItem.Name = "aBMsToolStripMenuItem";
+            this.aBMsToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.aBMsToolStripMenuItem.Text = "ABMs";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // configuracToolStripMenuItem
+            // 
+            this.configuracToolStripMenuItem.Name = "configuracToolStripMenuItem";
+            this.configuracToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.configuracToolStripMenuItem.Text = "Configuración";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.usuariosToolStripMenuItem.Text = "Usuarios";
+            // 
+            // palabrasToolStripMenuItem
+            // 
+            this.palabrasToolStripMenuItem.Name = "palabrasToolStripMenuItem";
+            this.palabrasToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.palabrasToolStripMenuItem.Text = "Palabras";
+            // 
+            // categoriaToolStripMenuItem
+            // 
+            this.categoriaToolStripMenuItem.Name = "categoriaToolStripMenuItem";
+            this.categoriaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.categoriaToolStripMenuItem.Text = "Categorias";
+            // 
+            // regionesToolStripMenuItem
+            // 
+            this.regionesToolStripMenuItem.Name = "regionesToolStripMenuItem";
+            this.regionesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.regionesToolStripMenuItem.Text = "Regiones";
+            // 
+            // institucionesToolStripMenuItem
+            // 
+            this.institucionesToolStripMenuItem.Name = "institucionesToolStripMenuItem";
+            this.institucionesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.institucionesToolStripMenuItem.Text = "Instituciones";
+            // 
+            // PBUtnFooter
+            // 
+            this.PBUtnFooter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.PBUtnFooter.BackColor = System.Drawing.Color.Transparent;
+            this.PBUtnFooter.Image = global::CapaPresentacion.Properties.Resources.Logo_UTN;
+            this.PBUtnFooter.Location = new System.Drawing.Point(751, 618);
+            this.PBUtnFooter.Name = "PBUtnFooter";
+            this.PBUtnFooter.Size = new System.Drawing.Size(151, 22);
+            this.PBUtnFooter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBUtnFooter.TabIndex = 1;
+            this.PBUtnFooter.TabStop = false;
+            // 
+            // PContenedor
+            // 
+            this.PContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PContenedor.Location = new System.Drawing.Point(3, 32);
+            this.PContenedor.Name = "PContenedor";
+            this.PContenedor.Size = new System.Drawing.Size(899, 580);
+            this.PContenedor.TabIndex = 2;
+            // 
+            // FrmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 643);
+            this.Controls.Add(this.TLPFondoInicio);
+            this.MainMenuStrip = this.MSInicio;
+            this.MinimumSize = new System.Drawing.Size(921, 682);
+            this.Name = "FrmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dici Juegos";
+            this.TLPFondoInicio.ResumeLayout(false);
+            this.TLPFondoInicio.PerformLayout();
+            this.MSInicio.ResumeLayout(false);
+            this.MSInicio.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBUtnFooter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TLPFondoInicio;
+        private System.Windows.Forms.MenuStrip MSInicio;
+        private System.Windows.Forms.ToolStripMenuItem aBMsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem palabrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem institucionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuracToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.PictureBox PBUtnFooter;
+        private System.Windows.Forms.Panel PContenedor;
+    }
+}
